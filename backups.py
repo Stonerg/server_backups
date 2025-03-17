0a9fe74d50614ebecbd38dee50b4c069d38bf144
Victor Frunze <101556108+Stonerg@users.noreply.github.com>
2025-03-17 11:59:00 +0200

    Update backups.py

--- backups.py
+++ backups.py
@@ -6,7 +6,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from dotenv import load_dotenv()
+from dotenv import load_dotenv
 # Constants
 load_dotenv()
 
